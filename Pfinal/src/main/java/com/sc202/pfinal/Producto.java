4c10da5b8382e663bd781150b5fa59c21b8c5941
DanGonzaG <dan.g.g@hotmail.es>
2022-11-11 22:13:14 -0600

    Se crea clase proyecto, atibutos y métodos

---
+++ Pfinal/src/main/java/com/sc202/pfinal/Producto.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.sc202.pfinal;
+
+/**
+ *
+ * @author DanGG
+ */
+public class Producto {
+    private String precio;
+    private String cantidad;
+    private String producto;
+    private Marca marca;
+    
+    
+    private Categoria categoria;
+
+    public String getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(String precio) {
+        this.precio = precio;
+    }
+
+    public String getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(String cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public String getProducto() {
+        return producto;
+    }
+
+    public void setProducto(String producto) {
+        this.producto = producto;
+    }
+
+   
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public Marca getMarca() {
+        return marca;
+    }
+
+    public void setMarca(Marca marca) {
+        this.marca = marca;
+    }
+    
+    
+    
+}
